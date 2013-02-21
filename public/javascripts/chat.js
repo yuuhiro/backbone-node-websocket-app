2108129cb26e0f2ed5c2c095b91f9913299011ac
yuuhiro <yuuhiro.harada@gmail.com>
2013-02-22 00:30:45 +0900

    design

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -27,16 +27,16 @@ $(function() {
 			'blur .edit_form': 'editDone'
 		},
 		initialize: function() {
-			this.$el.bind("dragstart", _.bind(this.dragStart, this));
-			this.$el.bind("dragend", _.bind(this.dragend, this));
-			this.$el.bind("mousedown", _.bind(this.mousedown, this));
-			this.$el.bind("mouseup", _.bind(this.mouseup, this));
+			this.$el.bind('dragstart', _.bind(this.dragStart, this));
+			this.$el.bind('dragend', _.bind(this.dragend, this));
+			this.$el.bind('mousedown', _.bind(this.mousedown, this));
+			this.$el.bind('mouseup', _.bind(this.mouseup, this));
 		},
 		render: function() {
 			var data = this.model.toJSON();
 			data.form_message = data.message;
 			var message = data.message;
-			data.message = message.replace(/(\n|\r)/g, "<br />");
+			data.message = message.replace(/(\n|\r)/g, '<br />');
 			var date = this.convertDate(data.date);
 			data.newDate = date;
 			if(data.isDrag){
@@ -84,15 +84,15 @@ $(function() {
 		mousedown: function(e) {
 			var className = e.target.className;
 			if((className !== 'message') && (className !== 'edit_form')){
-				this.$el.attr("draggable", "true");
+				this.$el.attr('draggable', 'true');
 				$(this.el).css('z-index', '100');
 			}
 		},
 		mouseup: function(e) {
-			this.$el.attr("draggable", "false");
+			this.$el.attr('draggable', 'false');
 		},
 		dragStart: function(e) {
-			this.$el.attr("draggable", "true");
+			this.$el.attr('draggable', 'true');
 		},
 		dragend: function(e) {
 			if (e.originalEvent) e = e.originalEvent;
@@ -102,7 +102,7 @@ $(function() {
 			var moveX = e.pageX;
 			$target.css({'position': 'absolute' , 'top': moveY, 'left': moveX});
 			this.model.save({top: moveY, left: moveX, isDrag: true}, {silent: true});
-			this.$el.attr("draggable", "false");
+			this.$el.attr('draggable', 'false');
 		}
 	});
 
@@ -111,7 +111,9 @@ $(function() {
 		el: $('body'),
 		events: {
 		  'click #sendMessasge': 'saveMessage',
-		  'click #deleteAll': 'deleteAll'
+		  'click #deleteAll': 'deleteAll',
+		  'focus #formMessage': 'createMode',
+		  'blur #formMessage': 'createDone'
 		},
 		initialize:function () {
 			this.limiter = 10;
@@ -121,12 +123,25 @@ $(function() {
 		   this.$el.bind('drop', _.bind(this.drop, this));
 		},
 		render: function() {
+			$('#sendMessasge').addClass('not_valid');
 			$('#messageList').empty();
+			var isEmpty = this.collection.length;
+			if(isEmpty === 0){
+				$('#deleteAll').addClass('not_valid');
+			}else{
+				$('#deleteAll').removeClass('not_valid');
+			}
 			this.collection.each(function(message) {
 				var view = new View({ model: message });
 				$('#messageList').prepend(view.render().el);
 			});
 			this.limit();
+		},
+		createMode: function() {
+			$('#sendMessasge').removeClass('not_valid');
+		},
+		createDone: function() {
+			$('#sendMessasge').addClass('not_valid');
 		},
 		saveMessage: function() {
 			var $form = $('#formMessage');
