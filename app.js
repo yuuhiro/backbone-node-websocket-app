5e911ea248a0b2dfd1c98d715b4b4d4337e04dfc
原田優弘 <yuuhiro@yuuAir.local>
2014-02-15 01:19:24 +0900

    use other mongoschema and backend

--- app.js
+++ app.js
@@ -4,31 +4,31 @@
  */
 
 var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path')
-  , mongoose = require('mongoose')
-  , backboneio = require('backbone.io');
+	, routes = require('./routes')
+	, user = require('./routes/user')
+	, http = require('http')
+	, path = require('path')
+	, mongoose = require('mongoose')
+	, backboneio = require('backbone.io');
 
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'ejs');
-  app.use(express.favicon());
-  app.use(express.logger('dev'));
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'public')));
+	app.set('port', process.env.PORT || 3000);
+	app.set('views', __dirname + '/views');
+	app.set('view engine', 'ejs');
+	app.use(express.favicon());
+	app.use(express.logger('dev'));
+	app.use(express.bodyParser());
+	app.use(express.methodOverride());
+	app.use(express.cookieParser('your secret here'));
+	app.use(express.session());
+	app.use(app.router);
+	app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
-  app.use(express.errorHandler());
+	app.use(express.errorHandler());
 });
 
 app.get('/', routes.index);
@@ -36,36 +36,54 @@ app.get('/users', user.list);
 
 var server = http.createServer(app);
 server.listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+	console.log("Express server listening on port " + app.get('port'));
 });
 
 
 // mongoose
 var Schema = mongoose.Schema;
-var UserSchema = new Schema({
-  message: String,
-  date: Date,
-  top: String,
-  left: String,
-  isDrag: String
+
+var MemoSchema = new Schema({
+	message : String,
+	date    : Date,
+	top     : String,
+	left    : String,
+	isDrag  : String
 });
-mongoose.model('User', UserSchema);
+
+var MemoSchema2 = new Schema({
+	message : String,
+	date    : Date,
+	top     : String,
+	left    : String,
+	isDrag  : String
+});
+
+mongoose.model('Memo', MemoSchema);
+mongoose.model('Memo2', MemoSchema2);
 var uri = process.env.MONGOHQ_URL || 'mongodb://127.0.0.1/chat_app';
 mongoose.connect(uri);
-var User = mongoose.model('User');
+var Memo = mongoose.model('Memo');
+var Memo2 = mongoose.model('Memo2');
 
 // Backbone.io
 var backend = backboneio.createBackend();
 backend.use(function(req, res, next) {
-  console.log(req.method);
-  next();
+	console.log(req.method);
+	next();
+});
+var backend2 = backboneio.createBackend();
+backend2.use(function(req, res, next) {
+	console.log(req.method);
+	next();
 });
 
 // ミドルウェアを設定（mangodb）
-backend.use(backboneio.middleware.mongooseStore(User));
+backend.use(backboneio.middleware.mongooseStore(Memo));
+backend2.use(backboneio.middleware.mongooseStore(Memo2));
 
-var io = backboneio.listen(server, { mybackend: backend });
-io.configure(function () { 
-  io.set("transports", ["xhr-polling"]); 
-  io.set("polling duration", 10); 
-});
+var io = backboneio.listen(server, {mybackend: backend, mybackend2: backend2});
+// io.configure(function () { 
+// 	io.set("transports", ["xhr-polling"]); 
+// 	io.set("polling duration", 10); 
+// });
--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -62,7 +62,7 @@ $(function() {
 			this.updata();
 		},
 		updata: function() {
-			var message = $(this.el).find('.edit_form').val();
+			var message = $this.$el.find('.edit_form').val();
 			var date = new Date();
 			this.model.save({message: message, date: date});
 		},
@@ -88,7 +88,7 @@ $(function() {
 			var className = $target[0].className;
 			if((className !== 'message') && (className !== 'edit_form')){
 				this.$el.attr('draggable', 'true');
-				$(this.el).css('z-index', '100');
+				this.$el.css('z-index', '100');
 				this.offsetX = e.currentTarget.offsetLeft;
 				this.offsetY = e.currentTarget.offsetTop;
 				console.log(this.offsetX + '/' + this.offsetY);
@@ -117,43 +117,46 @@ $(function() {
 
 	// アプリのビュー
 	var AppView = Backbone.View.extend({
-		el: $('body'),
+		el: '.memo_wrap',
 		events: {
-		  'click #sendMessasge': 'saveMessage',
-		  'click #deleteAll': 'deleteAll',
-		  'focus #formMessage': 'createMode',
-		  'blur #formMessage': 'createDone'
+		  'click .js_send_message': 'saveMessage',
+		  'click .js_delete_all': 'deleteAll',
+		  'focus .js_form_message': 'createMode',
+		  'blur .js_form_message': 'createDone'
 		},
 		initialize:function () {
 			this.limiter = 10;
-		   _.bindAll(this, 'render');
-		   this.collection.on('add change remove reset', this.render);
-		   this.$el.bind('dragover', _.bind(this.dragover, this));
-		   this.$el.bind('drop', _.bind(this.drop, this));
+			_.bindAll(this, 'render');
+			this.collection.on('add change remove reset', this.render);
+			this.$el.bind('dragover', _.bind(this.dragover, this));
+			this.$el.bind('drop', _.bind(this.drop, this));
+			this.collection.fetch();
 		},
 		render: function() {
-			$('#sendMessasge').addClass('not_valid');
-			$('#messageList').empty();
+			console.log(this.collection);
+			var that = this;
+			this.$el.find('.js_send_message').addClass('not_valid');
+			this.$el.find('.js_message_list').empty();
 			var isEmpty = this.collection.length;
 			if(isEmpty === 0){
-				$('#deleteAll').addClass('not_valid');
+				this.$el.find('.js_delete_all').addClass('not_valid');
 			}else{
-				$('#deleteAll').removeClass('not_valid');
+				this.$el.find('.js_delete_all').removeClass('not_valid');
 			}
 			this.collection.each(function(message) {
 				var view = new View({ model: message });
-				$('#messageList').prepend(view.render().el);
+				that.$el.find('.js_message_list').prepend(view.render().el);
 			});
 			this.limit();
 		},
 		createMode: function() {
-			$('#sendMessasge').removeClass('not_valid');
+			this.$el.find('.js_send_message').removeClass('not_valid');
 		},
 		createDone: function() {
-			$('#sendMessasge').addClass('not_valid');
+			this.$el.find('.js_send_message').addClass('not_valid');
 		},
 		saveMessage: function() {
-			var $form = $('#formMessage');
+			var $form = this.$el.find('.js_form_message');
 			var message = $form.val();
 			if(!message){
 				return false;
@@ -165,8 +168,8 @@ $(function() {
 		limit: function() {
 			var model_limit = this.collection.length;
 			if(model_limit > this.limiter){
-				$('#sendMessasge').remove();
-				$('#formMessage').after('<p class="limit_message">制限を超えました</p>');
+				$('.js_send_message').remove();
+				$('.js_form_message').after('<p class="limit_message">制限を超えました</p>');
 			}
 		},
 		deleteAll: function() {
@@ -190,7 +193,202 @@ $(function() {
 	});
 
 	var models = new Models();
-	models.fetch();
+	// models.fetch();
 	new AppView({collection: models});
+
+	//　モデルの設定
+	var Model2 = Backbone.Model.extend({
+		idAttribute: '_id'
+	});
+
+	// コレクションの設定
+	var Models2 = Backbone.Collection.extend({
+		model: Model2,
+		backend: 'mybackend2',
+		initialize: function() {
+			this.bindBackend();
+		}
+	});
+
+	// モデルのビュー
+	var View2 = Backbone.View.extend({
+		tagName: 'li',
+		className: 'message_data',
+		template : new EJS({url: 'javascripts/templates/view.ejs'}),
+		events: {
+			'click .delete': 'delete',
+			'dblclick .message': 'editMode',
+			'blur .edit_form': 'editDone'
+		},
+		initialize: function() {
+			_.bindAll(this, 'render', 'delete', 'editMode', 'editDone', 'updata', 'convertDate', 'mousedown', 'dragStart', 'dragend');
+			this.$el.bind('dragstart', _.bind(this.dragStart, this));
+			this.$el.bind('dragend', _.bind(this.dragend, this));
+			this.$el.bind('mousedown', _.bind(this.mousedown, this));
+			this.$el.bind('mouseup', _.bind(this.mouseup, this));
+		},
+		render: function() {
+			var data = this.model.toJSON();
+			data.form_message = data.message;
+			var message = data.message;
+			data.message = message.replace(/(\n|\r)/g, '<br />');
+			var date = this.convertDate(data.date);
+			data.newDate = date;
+			if(data.isDrag){
+				this.$el.css({'position': 'absolute', 'top': data.top + 'px', 'left': data.left + 'px'});
+			}
+			this.$el.html((this.template).render(data));
+			return this;
+		},
+		delete: function() {
+			this.model.destroy();
+		},
+		editMode: function(e) {
+			var $message = $(e.target);
+			var $edit_fomr = $(e.target).next();
+			var height = $message.height();
+			$edit_fomr.height(height);
+			$message.hide();
+			$edit_fomr.show().focus();
+		},
+		editDone: function(e) {
+			$(e.target).hide().prev().show();
+			this.updata();
+		},
+		updata: function() {
+			var message = this.$el.find('.edit_form').val();
+			var date = new Date();
+			this.model.save({message: message, date: date});
+		},
+		convertDate: function(date) {
+			var date = new Date(date);
+			var year = date.getFullYear()
+				, month = date.getMonth()
+				, day = date.getDate()
+				, hour = date.getHours()
+				, minute = date.getMinutes();
+			var newDate = {
+					'year': year
+				,	'month': month + 1
+				, 'day': day
+				, 'hour': hour
+				, 'minute': minute
+			}
+			return newDate;
+		},
+		mousedown: function(e) {
+			var $target = $(e.target);
+			var $currentTarget = $(e.currentTarget);
+			var className = $target[0].className;
+			if((className !== 'message') && (className !== 'edit_form')){
+				this.$el.attr('draggable', 'true');
+				this.$el.css('z-index', '100');
+				this.offsetX = e.currentTarget.offsetLeft;
+				this.offsetY = e.currentTarget.offsetTop;
+				console.log(this.offsetX + '/' + this.offsetY);
+			}
+		},
+		mouseup: function(e) {
+			this.$el.attr('draggable', 'false');
+		},
+		dragStart: function(e) {
+			this.$el.attr('draggable', 'true');
+		},
+		dragend: function(e) {
+			if (e.originalEvent) e = e.originalEvent;
+			console.log(e);
+			var $target = $(e.currentTarget);
+			var moveX = e.layerX;
+			var moveY = e.layerY - e.target.offsetHeight;
+			var positionX = this.offsetX + moveX;
+			var positionY = this.offsetY + moveY;
+			console.log(moveX + '/' + moveY + '-' + positionX + '/' + positionY);
+			$target.css({'position': 'absolute' , 'top': positionY, 'left': positionX});
+			this.model.save({top: positionY, left: positionX, isDrag: true}, {silent: true});
+			this.$el.attr('draggable', 'false');
+		}
+	});
+
+	// アプリのビュー
+	var AppView2 = Backbone.View.extend({
+		el: '.memo_wrap2',
+		events: {
+		  'click .js_send_message': 'saveMessage',
+		  'click .js_delete_all': 'deleteAll',
+		  'focus .js_form_message': 'createMode',
+		  'blur .js_form_message': 'createDone'
+		},
+		initialize:function () {
+			this.limiter = 10;
+			_.bindAll(this, 'render');
+			this.collection.on('add change remove reset', this.render);
+			this.$el.bind('dragover', _.bind(this.dragover, this));
+			this.$el.bind('drop', _.bind(this.drop, this));
+			this.collection.fetch();
+		},
+		render: function() {
+			console.log(this.collection);
+			var that = this;
+			this.$el.find('.js_send_message').addClass('not_valid');
+			this.$el.find('.js_message_list').empty();
+			var isEmpty = this.collection.length;
+			if(isEmpty === 0){
+				this.$el.find('.js_delete_all').addClass('not_valid');
+			}else{
+				this.$el.find('.js_delete_all').removeClass('not_valid');
+			}
+			this.collection.each(function(message) {
+				var view = new View2({ model: message });
+				that.$el.find('.js_message_list').prepend(view.render().el);
+			});
+			this.limit();
+		},
+		createMode: function() {
+			console.log("tetete");
+			this.$el.find('.js_send_message').removeClass('not_valid');
+		},
+		createDone: function() {
+			this.$el.find('.js_send_message').addClass('not_valid');
+		},
+		saveMessage: function() {
+			var $form = this.$el.find('.js_form_message');
+			var message = $form.val();
+			if(!message){
+				return false;
+			}
+			var date = new Date();
+			this.collection.create({message: message, date: date});
+			$form.val('');
+		},
+		limit: function() {
+			var model_limit = this.collection.length;
+			if(model_limit > this.limiter){
+				$('.js_send_message').remove();
+				$('.js_form_message').after('<p class="limit_message">制限を超えました</p>');
+			}
+		},
+		deleteAll: function() {
+			if(window.confirm('すべて削除されます')){
+				var cloneModels = _.clone(this.collection.models);
+				_.each(cloneModels, function(model) {
+					model.destroy();
+				});
+			}else{
+				return false;
+			}
+		},
+		dragover: function(e) {
+			if (e.originalEvent) e = e.originalEvent;
+			 e.preventDefault();
+		},
+		drop: function(e) {
+			if (e.originalEvent) e = e.originalEvent;
+			e.preventDefault();
+		}
+	});
+
+	var models2 = new Models2();
+	// models2.fetch();
+	new AppView2({collection: models2});
 
 });
